5cd804953bb4e994c35a0e931d48d6c2dcab12e1
Christopher Dakin <christopherdakin1@gmail.com>
2017-05-27 15:26:12 -0500

    format time, fixed lines, added ask/bid

--- app.js
+++ app.js
@@ -3,22 +3,36 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     height = 500 - margin.top - margin.bottom;
 
 // // parse the date / time
-// var parseTime = d3.timeParse("%H:%M:%S");
+var parseTime = d3.timeParse("%H:%M:%S.%L");
 //
 // set the ranges
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
+var y2 = d3.scaleLinear().range([height, 0]);
 
 // define the area
 var area = d3.area()
-    .x(function(d) { return x(d.bid); })
+    .x(function(d) { return x(d.timeStr); })
     .y0(height)
-    .y1(function(d) { return y(d.ask); });
+    .y1(function(d) { return y(d.bid); })
+    .curve(d3.curveStepAfter);
+
+var area2 =d3.area()
+  .x(function(d) { return x(d.timeStr) })
+  .y0(0)
+  .y1(function(d) { return y(d.ask); })
+  .curve(d3.curveStepAfter);
 
 // define the line
 var valueline = d3.line()
-    .x(function(d) { return x(d.bid); })
-    .y(function(d) { return y(d.ask); });
+    .x(function(d) { return x(d.timeStr); })
+    .y(function(d) { return y(d.ask); })
+    .curve(d3.curveStepAfter);
+
+var valueline2 = d3.line()
+    .x(function(d) { return x(d.timeStr); })
+    .y(function(d) { return y(d.bid); })
+    .curve(d3.curveStepAfter);
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
@@ -30,36 +44,45 @@ var svg = d3.select("body").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-
-
   const bboList = stock.bboList
   // format the data
-  // bboList.forEach(function(d) {
-  //     d.timeStr = parseTime(d.timeStr);
-  //     console.log(d.timeStr)
-  // });
+  console.log(stock)
+  bboList.forEach(function(d) {
+      d.timeStr = parseTime(d.timeStr);
+      // d.ask = d.ask / 10000;
+      // d.bid = d.bid / 10000;
+  });
 
   // scale the range of the data
-  x.domain(d3.extent(bboList, function(d) { return d.bid; }));
-  y.domain([0, d3.max(bboList, function(d) { return d.ask; })]);
-
+  x.domain(d3.extent(bboList, function(d) { return d.timeStr; }));
+  y.domain([d3.min(bboList, function(d) { return d.bid * 0.995 }), d3.max(bboList, function(d) { return d.ask * 1.005 ; })]);
   // add the area
     svg.append("path")
        .data([bboList])
        .attr("class", "area")
        .attr("d", area);
 
-  // add the valueline path.
+    svg.append("path")
+       .data([bboList])
+       .attr("class", "area2")
+       .attr("d", area2);
+       
+  // // add the valueline path.
   svg.append("path")
       .data([bboList])
       .attr("class", "line")
       .attr("d", valueline);
 
-  // add the X Axis
+  svg.append("path")
+    .data([bboList])
+    .attr("class", "line2")
+    .attr("d", valueline2)
+  //
+  // // add the X Axis
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
-
+  //
   // add the Y Axis
   svg.append("g")
-      .call(d3.axisLeft(y));
+      .call(d3.axisLeft(y).tickFormat(d3.format(".0f")));
