4a57fa0050133200c63dc5108f8590cd91036320
Christopher Dakin <christopherdakin1@gmail.com>
2017-05-27 17:51:06 -0500

    got brush zoom working properly

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
+    height = 500 - margin.top - margin.bottom,
+    k = height / width,
+    x0 = [-4.5, 4.5],
+    y0 = [-4.5 * k, 4.5 * k]
 // // parse the date / time
 var parseTime = d3.timeParse("%H:%M:%S.%L");
 //
 // set the ranges
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
-var y2 = d3.scaleLinear().range([height, 0]);
+
+//define axes
+var xAxis = d3.axisBottom(x);
+var yAxis = d3.axisLeft(y);
 
 // define the area
 var area = d3.area()
@@ -17,7 +22,7 @@ var area = d3.area()
     .y1(function(d) { return y(d.bid); })
     .curve(d3.curveStepAfter);
 
-var area2 =d3.area()
+var area2 = d3.area()
   .x(function(d) { return x(d.timeStr) })
   .y0(0)
   .y1(function(d) { return y(d.ask); })
@@ -34,16 +39,60 @@ var valueline2 = d3.line()
     .y(function(d) { return y(d.bid); })
     .curve(d3.curveStepAfter);
 
+
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    // .call(d3.zoom()
+    //   .scaleExtent([1, 10])
+    //   .on("zoom", function() {
+    //     console.log("zooming")
+    //     svg.attr("transform", d3.event.transform);
+    //     svg.select("xAxis").call(xAxis);
+    //     svg.select("yAxis").call(yAxis);
+    //   }))
+    .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
+//zoom stuff
 
+var brush = d3.brush().on("end", brushended),
+    idleTimeout,
+    idleDelay = 350;
+
+
+function brushended() {
+  var s = d3.event.selection;
+  if (!s) {
+    if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+    x.domain(x0);
+    y.domain(y0);
+  } else {
+    x.domain([s[0][0], s[1][0]].map(x.invert, x));
+    y.domain([s[1][1], s[0][1]].map(y.invert, y));
+    svg.select(".brush").call(brush.move, null);
+  }
+  zoom();
+}
+
+function idled() {
+  idleTimeout = null;
+}
+
+function zoom() {
+  var t = svg.transition().duration(750);
+  svg.select(".xAxis").transition(t).call(xAxis);
+  svg.select(".yAxis").transition(t).call(yAxis);
+  svg.select(".line").transition(t).attr("d", valueline);
+  svg.select(".line2").transition(t).attr("d", valueline2);
+  svg.select(".area").transition(t).attr("d", area);
+  svg.select(".area2").transition(t).attr("d", area2);
+}
+
+//////////
   const bboList = stock.bboList
   // format the data
   console.log(stock)
@@ -66,7 +115,7 @@ var svg = d3.select("body").append("svg")
        .data([bboList])
        .attr("class", "area2")
        .attr("d", area2);
-       
+
   // // add the valueline path.
   svg.append("path")
       .data([bboList])
@@ -81,8 +130,15 @@ var svg = d3.select("body").append("svg")
   // // add the X Axis
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .attr("class", "xAxis")
+      .call(xAxis);
   //
   // add the Y Axis
   svg.append("g")
-      .call(d3.axisLeft(y).tickFormat(d3.format(".0f")));
+      .attr("class", "yAxis")
+      .call(yAxis.tickFormat(d3.format(".0f")));
+
+// add brush
+svg.append("g")
+    .attr("class", "brush")
+    .call(brush);
